8d5e65331dfa3a8447fd0bed45a318946b81be74
James <s3943003@student.rmit.edu.au>
2025-02-14 20:33:37 +1100

    centers window

--- src/cpp/userInterface/app.cpp
+++ src/cpp/userInterface/app.cpp
@@ -7,6 +7,7 @@ wxIMPLEMENT_APP(App);
 bool App::OnInit() {
     MainFrame* mainFrame = new MainFrame("CMC-Market-Report");
     mainFrame->SetClientSize(800, 600);
+    mainFrame->Center();
     mainFrame->Show();
     return true;
 }
